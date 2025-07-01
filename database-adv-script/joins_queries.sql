d7b98f5abc08e56879bed2a1f03d17bfd1fd3260
fiyin <lolafiyin@gmail.com>
2025-07-01 16:05:09 +0100

    FULL OUTER JOIN query to retrieve all users and all bookings

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -28,3 +28,20 @@ SELECT
 FROM properties p
 LEFT JOIN reviews r ON p.property_id = r.property_id
 ORDER BY p.property_name, r.review_date DESC;
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings
+-- This will show all users (even those without bookings) and all bookings (even orphaned ones)
+-- Note: Some databases use FULL OUTER JOIN, others use FULL JOIN
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.booking_date,
+    b.check_in_date,
+    b.check_out_date,
+    b.total_amount
+FROM users u
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id
+ORDER BY u.last_name, u.first_name, b.booking_date DESC;
